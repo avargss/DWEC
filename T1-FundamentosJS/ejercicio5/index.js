74382993a690a3124bd0471927832821334c96d6
avargas <alevargasgarran@gmail.com>
2024-09-26 22:05:14 +0200

    Ejercicios 5 y 6

--- T1-FundamentosJS/ejercicio5/index.js
+++ T1-FundamentosJS/ejercicio5/index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const readline = require(`readline`);
 
 const rl = readline.createInterface({
@@ -5,19 +6,35 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question(`Escoge un més del año por su número: `, (mes) => {
+const meses31 = [1, 3, 5, 7, 8, 10, 12];
+const meses30 = [4, 6, 9, 11];
+
+rl.question(`Escoge un mes del año por su número: `, (mes) => {
     mes = parseInt(mes);
 
-    if (mes == 1, 3, 5, 7, 8, 10, 12) {
-        alert(`El mes seleccionado tiene 31 días.`);
-    } else if (mes == 4, 6, 9, 11) {
-        alert(`El més seleccionado tiene 30 días.`)
-    } else if (mes == 2) {
-        alert(`Febrero tiene 28 días. 29 en años bisiestos.`);
+    // Este método es efectivo pero no el más eficiente.
+    /* if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+        console.log(`El mes seleccionado tiene 31 días.`);
+    } else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+        console.log(`El mes seleccionado tiene 30 días.`)
+    } else if (mes === 2) {
+        console.log(`Febrero tiene 28 días. 29 en años bisiestos.`);
     } else {
-        alert(`No válido.`);
+        console.log(`No válido.`);
+    } */
+
+    // La forma más eficiente de hacer este código sería usando arrays que he definido arriba:
+    if (meses31.includes(mes)) {
+        console.log(`El mes seleccionado tiene 31 días.`);
+    } else if (meses30.includes(mes)) {
+        console.log(`El mes seleccionado tiene 30 días.`);
+    } else if (mes == 2) {
+        console.log(`Febrero tiene 28 días. 29 en años bisiestos.`);
+    } else {
+        console.log(`No válido.`);
+        
     }
 
-    rl.close;
+    rl.close();
 
 });
---
+++ T1-FundamentosJS/ejercicio6/index.js
@@ -0,0 +1,6 @@
+let frutas = [`Mango`, `Manzana`, `Naranja`, `Melón`, `Sandía`];
+
+frutas.forEach(function (elemento, indice, frutas) {
+    console.log(elemento, indice);
+    
+});
