172f8e24813f9e71f1a2bad775f95f31ce15f999
Alejandro Vargas Garrán <alevargasgarran@gmail.com>
2024-09-30 20:44:26 +0200

    Update Ejercicio4

--- T1-FundamentosJS/arrowFunctions/Ejercicio4.js
+++ T1-FundamentosJS/arrowFunctions/Ejercicio4.js
@@ -1,4 +1,15 @@
 let numeros = [1, 2, 3, 4, 5];
 
-const suma = numeros.reduce((acumulador));
+function arrayMin(numeros) {
+    return numeros.reduce(function (p, v) {
+        return (p > v ? p : v)
 
+    });
+}
+
+function arrayMax(numeros) {
+    return numeros.reduce(function (p, v) {
+        return (p < v ? p : v)
+
+    });
+}
