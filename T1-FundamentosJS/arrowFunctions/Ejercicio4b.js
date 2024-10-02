829043d4c669262ee8b9194b6d0a924e606a6b2c
avargas <alevargasgarran@gmail.com>
2024-10-02 09:40:48 +0200

    Corrección ejercicio 4b

--- T1-FundamentosJS/arrowFunctions/Ejercicio4b.js
+++ T1-FundamentosJS/arrowFunctions/Ejercicio4b.js
@@ -1,12 +1,23 @@
 // Haz un array de personas y muestra el nombre de aquellos que sean mayores de 18.
 
-let personas = [ 
-    { nombre: `Andrés`, edad: 30},
+let personas = [
+    { nombre: `Andrés`, edad: 30 },
     { nombre: `Juan`, edad: 25 },
     { nombre: `Miguel`, edad: 18 },
     { nombre: `Rodolfo`, edad: 15 },
 ];
 
-const mayoresDeEdad = personas.filter((personas) => personas.edad >= 18);
+/* const mayoresDeEdad = personas
+    .filter((personas) => personas.edad >= 18)
+    .forEach(p => console.log(p.nombre));
+ */
 
-console.log(mayoresDeEdad);
+    
+personas    // Se puede reducir a esto
+    .filter((personas) => personas.edad >= 18)
+    .forEach(p => console.log(p.nombre));
+
+/* mayoresDeEdad.forEach(p => {
+    console.log(p.nombre);
+    
+}); */
