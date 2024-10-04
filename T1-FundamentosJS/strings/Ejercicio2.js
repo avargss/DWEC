1d9f2b615cb2e231bb223136e236315749fb17d4
avargas <alevargasgarran@gmail.com>
2024-10-04 09:00:18 +0200

    Cambios menores y correcciones de nombres

--- T1-FundamentosJS/strings/Ejercicio2.js
+++ T1-FundamentosJS/strings/Ejercicio2.js
@@ -4,23 +4,23 @@ function enviarEmail(nombre, edad, mensaje, correo) {
         return null;
     }
 
-    // Determinar el estado del usuario
-    const estado = edad >= 18
+    // Comprobar si el usuario es válido o no
+    let estado = edad >= 18
         ? `${nombre} es un usuario válido`
         : `${nombre} no es un usuario válido`;
 
     // Crear el mensaje del correo
-    const mensajeEmail = `A user has posted a comment from the website:\n` +
-                         `Nombre: ${nombre}\n` +
-                         `Edad: ${edad}\n` +
-                         `Estado: ${estado}\n` +
-                         `Mensaje: ${mensaje}\n` +
-                         `Correo: ${correo}`;
+    const mensajeEmail = `${nombre} ${edad >= 18 ? " es un usuario válido" : " no es un usuario válido"}`; // Ambos métodos son correctos pero a Nuria le gusta más este.
+                        /*`Un usuario ha enviado un nuevo mensaje:
+                         Nombre: ${nombre}
+                         Edad: ${edad}
+                         Estado: ${estado}
+                         Mensaje: ${mensaje}
+                         Correo: ${correo};`*/
 
     // Devolver el mensaje del correo
     return mensajeEmail;
 }
 
-// Ejemplo de uso
-const resultado = enviarEmail("Knekro", 20, "Mondongo, god no?", "knekro@example.com");
+let resultado = enviarEmail("Knekro", 20, "Mondongo, god no?", "knekro@example.com");
 console.log(resultado);
