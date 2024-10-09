86a768900ed0f4993e80ef483502b411e85139a5
avargas <alevargasgarran@gmail.com>
2024-10-09 17:40:56 +0200

    Ejercicio CSV

---
+++ t2-FundamentosJS/ejercicioCSV/datosCSV.js
@@ -0,0 +1,7 @@
+const clase = "Miercoles";
+
+const csv = `shipment.shipmentIdentifier,shipmentLineNumber,shipment.shipmentType,order.orderIdentifier,orderLine.orderLineNumber,dateCreated,quantity,quantityUnits,value,valueCurrency`;
+const linea1 = `3004539,100,OUTBOUND,100044355,100,2021-07-05T00:00:00,400,EA,500000,USD`;
+const linea2 = `3004539,200,OUTBOUND,100044355,200,2021-07-05T00:00:00,400,EA,0,USD`;
+const linea3 = `3004539,300,OUTBOUND,100044355,300,2021-07-05T00:00:00,400,EA,0,USD`;
+const linea4 = `3004540,100,OUTBOUND,100044354,100,2021-07-05T00:00:00,208,EA,260000,USD`;
