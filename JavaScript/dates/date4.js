207da12b634f986f06e393809dabe1dcb8e54a84
Alejandro Vargas Garrán <alevargasgarran@gmail.com>
2024-10-29 23:30:47 +0100

    Date 4

---
+++ JavaScript/dates/date4.js
@@ -0,0 +1,32 @@
+function dias() {
+    const fechaHoy = new Date("10/29/2024");
+    const sept15 = new Date("09/10/2024");
+
+    let milisDia = 24 * 60 * 60 * 1000;
+    let milisPas = Math.abs(fechaHoy.getTime() - sept15.getTime());
+
+    let diff = Math.round(milisPas / milisDia)
+
+    return diff;
+}
+
+console.log(`Días desde el 15 de septiembre: ${dias()}`);
+
+function lunes() {
+    const sept15 = new Date("2024-09-15");
+    const hoy = new Date();
+    let contador = 0;
+
+    while (sept15 <= hoy) {
+        if(sept15.getDay() === 1) {
+            contador++;
+        }
+
+        sept15.setDate(sept15.getDate() + 1)
+    }
+
+    return contador;
+
+}
+
+console.log(`Cantidad de lunes desde el 15 de septiembre: ${lunes()}`);
