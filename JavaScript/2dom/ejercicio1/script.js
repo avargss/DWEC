8190fb2e940909c4ba766d87f9272d89dc7318fc
avargas <alevargasgarran@gmail.com>
2024-11-05 17:09:18 +0100

    Modif ej1 dom2

--- JavaScript/2dom/ejercicio1/script.js
+++ JavaScript/2dom/ejercicio1/script.js
@@ -1,12 +1,12 @@
 function obtainFamilyNodes(event) {
     const element = event.target;
 
-    console.log("Elemento elegido: ", element.tagName, element.id || "No tiene id");
+    console.log("Elemento elegido: ", element.tagName, element.id || ", No tiene id");
 
     // Nodo padre
 
     const father = element.parentElement;
-    console.log("Padre: ", father.tagName, father.id || "No tiene id");
+    console.log("Padre: ", father.tagName, father.id || ", No tiene id");
 
     // Nodos hermanos
 
@@ -35,11 +35,26 @@ function obtainFamilyNodes(event) {
     // Hermanos del padre
 
     const fatherSiblings = Array.from(grandfather.children).filter(fSibligs => fSibligs !== father);
-    console.log("Hermanos del padre:", fatherSiblings.map(sibling => `${sibling.tagName} ${sibling.id || "Sin ID"}`).join(", ") || "No hay hermanos del padre");
+    console.log("Hermanos del padre:", fatherSiblings.map(sibling => `${sibling.tagName} ${sibling.id || ", No tiene ID"}`).join(", ") || "No hay hermanos del padre");
     
     // Primos
 
+    const cousins = fatherSiblings.children;
+    console.log("Primos: ", cousins ? `${cousins.tagName}, ${cousins.id || "No tiene id"}` : "No tiene primos");
+
+    // Hijos
+
+    const childrens = element.children;
+    console.log("Hijos: ", childrens ? `${childrens.tagName}, ${childrens.id || "No tiene id"}` : "No tiene hijos");    
+
+    // Nietos
+
+    const grandChildren = childrens.children;
+    console.log("Nietos: ", grandChildren ? `${grandChildren.tagName}, ${grandChildren.id || "No tiene id"}` : "No tiene nietos");
     
-
-
+    console.log("=======================================================");
+    
 }
+
+document.querySelector("#child2").addEventListener("click", obtainFamilyNodes)
+document.querySelector("#child3").addEventListener("click", obtainFamilyNodes)
