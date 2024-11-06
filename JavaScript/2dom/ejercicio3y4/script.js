e84bc0e028e32293c0749f6bfe324bc3c5c29602
Alejandro Vargas Garrán <alevargasgarran@gmail.com>
2024-11-06 17:24:05 +0100

    Rename ejs3 y 4

--- JavaScript/2dom/ejercicio3y4/script.js
+++ JavaScript/2dom/ejercicio3y4/script.js
@@ -7,9 +7,11 @@ function replaceTabooWords() {
     // Uso el some para que si words.textContent tiene alguna palabra del array, la reemplaza por "Contenido bloqueado";
     search.forEach(words => {
 
+        // Ejercicio 3
         if (tabooWords.some(taboo => words.textContent.includes(taboo))) {
 
             console.log("works");
+            // Ejercicio 4
             words.textContent = "Contenido bloqueado";
         }
     });
