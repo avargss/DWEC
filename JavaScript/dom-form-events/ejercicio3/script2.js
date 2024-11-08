9a2c89859f9b6993832f235b060bde145d85a075
Alejandro Vargas Garrán <alevargasgarran@gmail.com>
2024-11-09 00:33:51 +0100

    Creacion del form y metodos select

---
+++ JavaScript/dom-form-events/ejercicio3/script2.js
@@ -0,0 +1,10 @@
+// Formulario de persona nueva.
+
+let submitButton = document.getElementById("submit");
+
+
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault(); // Evitar el envío del formulario
+
+});
