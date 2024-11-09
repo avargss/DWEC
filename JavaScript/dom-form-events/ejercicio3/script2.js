f1faeebeef46a7eb201cde00f63bef6e66f75cba
Alejandro Vargas Garrán <alevargasgarran@gmail.com>
2024-11-09 16:14:52 +0100

    Campos validados, falta meter expresiones regulares en dni y telefono

--- JavaScript/dom-form-events/ejercicio3/script2.js
+++ JavaScript/dom-form-events/ejercicio3/script2.js
@@ -1,10 +1,38 @@
 // Formulario de persona nueva.
 
-let submitButton = document.getElementById("submit");
+const doc = document.getElementById("documentacion");
+const dni = document.getElementById("dni");
+const telefono = document.getElementById("telefono")
+const submitButton = document.getElementById("submit");
+
+const regExpDni = /^\d{8}[A-Z]$/;
+const regExpTelf = /^\d{9}$/;
+
+// El input necesita un required para que lo valide bien, si no lo pone como correcto aunque no tenga nada introducido.
+function validacion() {
+
+    const forms = document.querySelectorAll('.needs-validation');
 
 
+    forms.forEach(form => {
+        submitButton.addEventListener('click', event => {
 
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault(); // Evitar el envío del formulario
+            event.preventDefault(); // Evitar el envío del formulario.
+            
+            if (!regExpDni.test(dni)) {
+                dni.classList.add("is-invalid");
+            }
 
-});
+            if (form.checkValidity()) {
+                // Devuelve true si todos los campos son correctos
+                console.log("works");
+            }
+
+            form.classList.add("was-validated");
+
+        });
+    })
+
+}
+
+validacion();
