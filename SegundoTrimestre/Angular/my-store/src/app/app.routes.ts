c0b0537543251d5992c9a28b3b6fd809e11af84e
avargas <alevargasgarran@gmail.com>
2025-01-21 14:41:20 +0100

    my-store con Angular

---
+++ SegundoTrimestre/Angular/my-store/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { product-list } from './product-list/product-list.component';
+
+export const routes: Routes = [
+{ path: 'shop', component: product-list },
+];
